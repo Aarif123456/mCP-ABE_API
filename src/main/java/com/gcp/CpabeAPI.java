4474b7557b6816fcf31aa3ddebe2c06714e3af7c
Abdullah Arif <arif115@uwindsor.ca>
2021-03-07 01:40:37 -0500

    Got API working with post requests

--- src/main/java/com/gcp/CpabeAPI.java
+++ src/main/java/com/gcp/CpabeAPI.java
@@ -1,17 +1,4 @@
-/*
 
-import com.google.cloud.functions.HttpFunction;
-import com.google.cloud.functions.HttpRequest;
-import com.google.cloud.functions.HttpResponse;
-import java.io.BufferedWriter;
-
-public class Example implements HttpFunction {
-  @Override
-  public void service(HttpRequest request, HttpResponse response) throws Exception {
-    BufferedWriter writer = response.getWriter();
-    writer.write("Hello world!");
-  }
-}*/
 package com.gcp;
 import com.google.cloud.functions.HttpFunction;
 import com.google.cloud.functions.HttpRequest;
@@ -31,6 +18,8 @@ import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+
 import static com.mitu.cpabe.Cpabe.*;
 
 public class CpabeAPI implements HttpFunction {
@@ -46,19 +35,15 @@ public class CpabeAPI implements HttpFunction {
         // Default values avoid null issues (with switch/case) and exceptions from get() (optionals)
         String contentType = request.getContentType().orElse("");
         var writer = new PrintWriter(response.getWriter());
-        String method = null;
+        String method;
         switch (contentType) {
             case "application/json":
                 JsonObject body = gson.fromJson(request.getReader(), JsonObject.class);
                 method = respondMethod(body, writer);
                 break;
-            /*case "application/x-www-form-urlencoded":
-                // 'name=John' in the body of a POST request (not the URL)
-                Optional<String> nameParam = request.getFirstQueryParameter("name");
-                if (nameParam.isPresent()) {
-                    name = nameParam.get();
-                }
-                break;*/
+            case "application/x-www-form-urlencoded":
+                method = respondQuery(request, writer);
+                break;
             default:
                 // Invalid or missing "Content-Type" header
                 response.setStatusCode(HttpURLConnection.HTTP_UNSUPPORTED_TYPE);
@@ -70,6 +55,125 @@ public class CpabeAPI implements HttpFunction {
             response.setStatusCode(HttpURLConnection.HTTP_BAD_REQUEST);
         }
 
+    }
+
+    private String respondQuery(HttpRequest request, PrintWriter writer) {
+        var method = "";
+        Gson gson =new Gson();
+        JsonObject js = new JsonObject();
+        Optional<String> methodParam = request.getFirstQueryParameter("method");
+        if (methodParam.isPresent()) {
+            method = methodParam.get();
+            switch(method){
+                case "setup":
+                    Optional<String> attributeUniverseParam = request.getFirstQueryParameter("attributeUniverse");
+                    if(attributeUniverseParam.isPresent()){
+                        String[] attributeUniverse = gson.fromJson(attributeUniverseParam.get(), String[].class);
+                        js = setup(attributeUniverse);
+                    } else {
+                        js.addProperty("Error", "Missing argument for the setup function");
+                    }
+                    break;
+
+                case "keygen":{
+                    Optional<String> publicKeyParam = request.getFirstQueryParameter("publicKey");
+                    Optional<String> masterKeyParam = request.getFirstQueryParameter("masterKey");
+                    Optional<String> userAttributesParam = request.getFirstQueryParameter("userAttributes");
+                    if(publicKeyParam.isPresent() && masterKeyParam.isPresent() && userAttributesParam.isPresent() ){
+                        var publicKey = publicKeyParam.get();
+                        var masterKey = masterKeyParam.get();
+                        var userAttributes = userAttributesParam.get();
+                        js = keygen(publicKey, masterKey, userAttributes);
+                    }
+                    else{
+                        js.addProperty("Error", "Missing argument for the keygen function");
+                    }
+                    break;
+                }
+                case "encrypt": {
+                    Optional<String> publicKeyParam = request.getFirstQueryParameter("publicKey");
+                    Optional<String> policyParam = request.getFirstQueryParameter("policy");
+                    Optional<String> inputFileParam = request.getFirstQueryParameter("inputFile");
+                    if(publicKeyParam.isPresent() && policyParam.isPresent() && inputFileParam.isPresent() ) {
+                        var publicKey = publicKeyParam.get();
+                        var policy = policyParam.get();
+                        var inputFile = inputFileParam.get();
+                        try {
+                            js = encrypt(publicKey, policy, inputFile);
+                        } catch (IOException |
+                                NoSuchPaddingException |
+                                NoSuchAlgorithmException |
+                                InvalidKeyException |
+                                IllegalBlockSizeException |
+                                BadPaddingException e) {
+                            e.printStackTrace();
+                            js.addProperty("Error", e.getMessage());
+                        }
+                    } else{
+                        js.addProperty("Error", "Missing argument for encrypt function");
+                    }
+                    break;
+                }
+                case "halfDecrypt":{
+                    Optional<String> publicKeyParam = request.getFirstQueryParameter("publicKey");
+                    Optional<String> share1Param = request.getFirstQueryParameter("share1");
+                    Optional<String> encryptedFileParam = request.getFirstQueryParameter("encryptedFile");
+                    Optional<String> professionalIdParam = request.getFirstQueryParameter("professionalId");
+                    if(publicKeyParam.isPresent() && share1Param.isPresent() &&
+                            encryptedFileParam.isPresent()  && professionalIdParam.isPresent()) {
+                        var publicKey = publicKeyParam.get();
+                        var share1 = share1Param.get();
+                        var encryptedFile = encryptedFileParam.get();
+                        var professionalId = professionalIdParam.get();
+                        try {
+                            js = halfDecrypt(publicKey, share1, encryptedFile, professionalId);
+                        } catch (IOException |
+                                AttributesNotSatisfiedException |
+                                NoSuchDecryptionTokenFoundException e) {
+                            e.printStackTrace();
+                            js.addProperty("Error", e.getMessage());
+                        }
+                    } else{
+                        js.addProperty("Error", "Missing argument for the half-decrypt function");
+                    }
+                    break;
+                }
+                case "decrypt": {
+                    Optional<String> publicKeyParam = request.getFirstQueryParameter("publicKey");
+                    Optional<String> share2Param = request.getFirstQueryParameter("share2");
+                    Optional<String> encryptedFileParam = request.getFirstQueryParameter("encryptedFile");
+                    Optional<String> mDecryptedFileParam = request.getFirstQueryParameter("mDecryptedFile");
+                    Optional<String> professionalIdParam = request.getFirstQueryParameter("professionalId");
+                    if(publicKeyParam.isPresent() && share2Param.isPresent() &&
+                            encryptedFileParam.isPresent()  && mDecryptedFileParam.isPresent() &&
+                            professionalIdParam.isPresent()) {
+                        var publicKey = publicKeyParam.get();
+                        var share2 = share2Param.get();
+                        var encryptedFile = encryptedFileParam.get();
+                        var mDecryptedFile = mDecryptedFileParam.get();
+                        var professionalId = professionalIdParam.get();
+                        try {
+                            js = decrypt(publicKey, share2, encryptedFile, mDecryptedFile, professionalId);
+                        } catch (IllegalBlockSizeException |
+                                NoSuchAlgorithmException |
+                                IOException |
+                                BadPaddingException |
+                                NoSuchPaddingException |
+                                InvalidKeyException e) {
+                            js.addProperty("Error", e.getMessage());
+                        }
+                    } else{
+                        js.addProperty("Error", "Missing argument for the decrypt function");
+                    }
+
+                    break;
+                }
+                default:
+                    js.addProperty("Error", "Method type not recognized of type\""+method+"\"");
+            }
+        }
+        writer.write(gson.toJson(js));
+        return method;
     }
 
     private String respondMethod(JsonObject body, PrintWriter writer) {
