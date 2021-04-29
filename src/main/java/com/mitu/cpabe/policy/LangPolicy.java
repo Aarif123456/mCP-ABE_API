f9c9ea6a49ec9a872068ad9f85122a2a34a6a83c
Abdullah Arif <arif115@uwindsor.ca>
2021-04-29 05:42:47 -0400

    Simplified string sorter

--- src/main/java/com/mitu/cpabe/policy/LangPolicy.java
+++ src/main/java/com/mitu/cpabe/policy/LangPolicy.java
@@ -1,24 +1,17 @@
-/*
- * 
- */
 package com.mitu.cpabe.policy;
 
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.StringTokenizer;
 
-// TODO: Auto-generated Javadoc
-/**
- * The Class LangPolicy.
- */
+/* The class that outlines the grammar of the ciphertext policy */
 public class LangPolicy {
 
 	/**
 	 * Parses the attribute.
 	 * 
 	 * @author Mitu Kumar Debnath
-	 * @param s
-	 *            the s
+	 * @param s the string of attributes to be parsed
 	 * @return the string[]
 	 */
 	public static String[] parseAttribute(String s) {
@@ -34,8 +27,7 @@ public class LangPolicy {
 
 			str_arr.add(token);
 		}
-
-		str_arr.sort(new SortByAlphabetic());
+		str_arr.sort(String::compareTo);
 
 		len = str_arr.size();
 		res = new String[len];
@@ -43,25 +35,5 @@ public class LangPolicy {
 			res[i] = str_arr.get(i);
 		}
 		return res;
-	}
-
-	/**
-	 * The Class SortByAlphabetic.
-	 */
-	static class SortByAlphabetic implements Comparator<String> {
-
-		/*
-		 * (non-Javadoc)
-		 * 
-		 * @see java.util.Comparator#compare(java.lang.Object, java.lang.Object)
-		 */
-		@Override
-		public int compare(String s1, String s2) {
-			if (s1.compareTo(s2) >= 0) {
-				return 1;
-			}
-			return 0;
-		}
-
 	}
 }
