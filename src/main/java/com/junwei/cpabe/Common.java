74f510eb871b7466c292efb84a4c0a5c91b149ea
Abdullah Arif <arif115@uwindsor.ca>
2021-05-08 03:53:04 -0400

    Removed extra code from Common.java

--- src/main/java/com/junwei/cpabe/Common.java
+++ src/main/java/com/junwei/cpabe/Common.java
@@ -1,138 +1,58 @@
 package com.junwei.cpabe;
+
 import java.io.ByteArrayOutputStream;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 
 public class Common {
 
-	/* read byte[] from inputfile */
-	public static byte[] suckFile(String inputfile) throws IOException {
-		InputStream is = new FileInputStream(inputfile);
-		int size = is.available();
-		byte[] content = new byte[size];
+    /**
+     * Return a ByteArrayOutputStream instead of writing to a file
+     */
+    public static ByteArrayOutputStream writeCpabeData(byte[] cphBuf,
+                                                       byte[] aesBuf) throws IOException {
+        int i;
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
 
-		is.read(content);
+        /* write aes_buf */
+        for (i = 3; i >= 0; i--)
+            os.write(((aesBuf.length & (0xff << 8 * i)) >> 8 * i));
+        os.write(aesBuf);
 
-		is.close();
-		return content;
-	}
+        /* write cph_buf */
+        for (i = 3; i >= 0; i--)
+            os.write(((cphBuf.length & (0xff << 8 * i)) >> 8 * i));
+        os.write(cphBuf);
 
-	/* write byte[] into outputfile */
-	public static void spitFile(String outputfile, byte[] b) throws IOException {
-		OutputStream os = new FileOutputStream(outputfile);
-		os.write(b);
-		os.close();
-	}
+        os.close();
+        return os;
+    }
 
+    /**
+     * Read data from an InputStream instead of taking it from a file.
+     */
+    public static byte[][] readCpabeData(InputStream is) throws IOException {
+        int i, len;
 
-	public static void writeCpabeFile(String encfile,
-			byte[] cphBuf, byte[] aesBuf) throws IOException {
-		int i;
-		OutputStream os = new FileOutputStream(encfile);
+        byte[][] res = new byte[2][];
+        byte[] aesBuf, cphBuf;
 
-		/* write aes_buf */
-		for (i = 3; i >= 0; i--)
-			os.write(((aesBuf.length & (0xff << 8 * i)) >> 8 * i));
-		os.write(aesBuf);
+        len = 0;
+        for (i = 3; i >= 0; i--)
+            len |= is.read() << (i * 8);
+        aesBuf = new byte[len];
+        is.read(aesBuf);
 
-		/* write cph_buf */
-		for (i = 3; i >= 0; i--)
-			os.write(((cphBuf.length & (0xff << 8 * i)) >> 8 * i));
-		os.write(cphBuf);
+        /* read cph buf */
+        len = 0;
+        for (i = 3; i >= 0; i--)
+            len |= is.read() << (i * 8);
+        cphBuf = new byte[len];
+        is.read(cphBuf);
 
-		os.close();
-
-	}
-
-	public static byte[][] readCpabeFile(String encfile) throws IOException {
-		int i, len;
-		InputStream is = new FileInputStream(encfile);
-		byte[][] res = new byte[2][];
-		byte[] aesBuf, cphBuf;
-
-		/* read aes buf */
-		len = 0;
-		for (i = 3; i >= 0; i--)
-			len |= is.read() << (i * 8);
-		aesBuf = new byte[len];
-
-		is.read(aesBuf);
-
-		/* read cph buf */
-		len = 0;
-		for (i = 3; i >= 0; i--)
-			len |= is.read() << (i * 8);
-		cphBuf = new byte[len];
-
-		is.read(cphBuf);
-
-		is.close();
-
-		res[0] = aesBuf;
-		res[1] = cphBuf;
-		return res;
-	}
-	/**
-	 * Return a ByteArrayOutputStream instead of writing to a file
-	 */
-	public static ByteArrayOutputStream writeCpabeData(byte[] mBuf,
-			byte[] cphBuf, byte[] aesBuf) throws IOException {
-		int i;
-		ByteArrayOutputStream os = new ByteArrayOutputStream();
-		/* write m_buf */
-		for (i = 3; i >= 0; i--)
-			os.write(((mBuf.length & (0xff << 8 * i)) >> 8 * i));
-		os.write(mBuf);
-
-		/* write aes_buf */
-		for (i = 3; i >= 0; i--)
-			os.write(((aesBuf.length & (0xff << 8 * i)) >> 8 * i));
-		os.write(aesBuf);
-
-		/* write cph_buf */
-		for (i = 3; i >= 0; i--)
-			os.write(((cphBuf.length & (0xff << 8 * i)) >> 8 * i));
-		os.write(cphBuf);
-
-		os.close();
-		return os;
-	}
-	/**
-	 * Read data from an InputStream instead of taking it from a file.
-	 */
-	public static byte[][] readCpabeData(InputStream is) throws IOException {
-		int i, len;
-		
-		byte[][] res = new byte[3][];
-		byte[] mBuf, aesBuf, cphBuf;
-
-		/* read m buf */
-		len = 0;
-		for (i = 3; i >= 0; i--)
-			len |= is.read() << (i * 8);
-		mBuf = new byte[len];
-		is.read(mBuf);
-		/* read aes buf */
-		len = 0;
-		for (i = 3; i >= 0; i--)
-			len |= is.read() << (i * 8);
-		aesBuf = new byte[len];
-		is.read(aesBuf);
-
-		/* read cph buf */
-		len = 0;
-		for (i = 3; i >= 0; i--)
-			len |= is.read() << (i * 8);
-		cphBuf = new byte[len];
-		is.read(cphBuf);
-
-		is.close();
-		res[0] = aesBuf;
-		res[1] = cphBuf;
-		res[2] = mBuf;
-		return res;
-	}
+        is.close();
+        res[0] = aesBuf;
+        res[1] = cphBuf;
+        return res;
+    }
 }
